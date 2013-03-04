a752a07c804553265f8b4a14ec2cfa3a1db8048a
Cassiano Leal <cassianoleal@gmail.com>
2013-03-04 16:48:27 -0300

    Get chef_server_url from Vagrantfile instead of knife.rb

--- lib/vagrant-butcher/cleanup.rb
+++ lib/vagrant-butcher/cleanup.rb
@@ -6,8 +6,8 @@ module Vagrant
         def knife_config; @knife_config || "#{ENV['HOME']}/.chef/knife.rb"; end
       end
 
-      def cleanup_chef_server(node_name)
-        chef_api = ::Chef::REST.new(::Chef::Config[:chef_server_url])
+      def cleanup_chef_server(chef_server_url, node_name)
+        chef_api = ::Chef::REST.new(chef_server_url)
         %w(node client).each do |resource|
           env[:ui].info "Removing Chef #{resource} \"#{node_name}\"..."
           begin
@@ -19,11 +19,16 @@ module Vagrant
       end
 
       def cleanup
-        ::Chef::Config.from_file(config.knife_config)
+        require 'pry-debugger';binding.pry
         conf = env[:vm].config.vm
 
-        # If a node_name is given to the chef-client provisioner, use it
         chef_provisioner = conf.provisioners.find { |p| p.provisioner.name == "Vagrant::Provisioners::ChefClient" }
+        chef_server_url = chef_provisioner.config.chef_server_url
+
+        # Gets client_key_path from knife.rb unless set on the Vagrantfile
+        ::Chef::Config.from_file(config.knife_config) unless chef_provisioner.config.respond_to? :client_key_path
+        
+        # If a node_name is given to the chef-client provisioner, use it
         victim = chef_provisioner.config.node_name
 
         # If no node_name, fall back to host_name or box
@@ -31,8 +36,8 @@ module Vagrant
           env[:ui].info "No chef.node_name set, falling back to vm.host_name or vm.box."
           victim ||= conf.host_name || conf.box
         end
-
-        cleanup_chef_server(victim)
+        
+        cleanup_chef_server(chef_server_url, victim)
       end
     end
   end
--- lib/vagrant-butcher/version.rb
+++ lib/vagrant-butcher/version.rb
@@ -1,5 +1,5 @@
 module Vagrant
   module Butcher
-    VERSION = "0.0.3"
+    VERSION = "0.0.4.pre"
   end
 end
