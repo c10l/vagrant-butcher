741ca706daac894f6def7db544d0806a3bb47735
Cassiano Leal <cassianoleal@gmail.com>
2013-12-11 13:57:32 -0200

    Fix / add specs

--- spec/unit/vagrant_butcher/config_spec.rb
+++ spec/unit/vagrant_butcher/config_spec.rb
@@ -3,16 +3,36 @@ require 'spec_helper.rb'
 describe Vagrant::Butcher::Config do
   subject { described_class.new }
 
-  it "does not have the option to set knife.rb path" do
-    subject.should_not respond_to(:knife_config_file)
+  it "permits the user to (en|dis)able the plugin" do
+    subject.should respond_to(:enabled=)
   end
 
   it "has the option to set guest chef client pem path" do
-    subject.should respond_to(:guest_key_path)
+    subject.should respond_to(:guest_key_path=)
   end
 
-  it "has the option to set cache dir path" do
-    subject.should respond_to(:cache_dir)
+  it "has the option to verify SSL certs" do
+    subject.should respond_to(:verify_ssl=)
+  end
+
+  it "allows the user to configure a proxy" do
+    subject.should respond_to(:proxy=)
+  end
+
+  it "has an option to set a custom client_name" do
+    subject.should respond_to(:client_name=)
+  end
+
+  it "has an option to point to a local client key" do
+    subject.should respond_to(:client_key=)
+  end
+
+  it "does not have the option to set knife.rb path" do
+    subject.should_not respond_to(:knife_config_file=)
+  end
+
+  it "does not have the option to set cache dir path" do
+    subject.should_not respond_to(:cache_dir=)
   end
 
   it "sets guest chef client pem default path" do
@@ -20,8 +40,8 @@ describe Vagrant::Butcher::Config do
     subject.guest_key_path.should eql('/etc/chef/client.pem')
   end
 
-  it "sets cache dir default path" do
+  it "sets cache dir path" do
     subject.finalize!
-    subject.cache_dir.should eql(File.expand_path(".vagrant/butcher"))
+    subject.cache_dir.should eql(".vagrant/butcher")
   end
 end
