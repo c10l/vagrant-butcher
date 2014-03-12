80c87eaef0fa8ab17362b9eaad41033dd2cccbe7
Shawn Neal <sneal@sneal.net>
2014-03-12 14:39:50 -0700

    Added support for the Vagrant 1.5 UI object
    The UI object in Vagrant 1.5 takes no constructor arguments.

--- lib/vagrant-butcher/env.rb
+++ lib/vagrant-butcher/env.rb
@@ -4,7 +4,11 @@ module Vagrant
       attr_accessor :ui
 
       def initialize
-        if Gem::Version.new(::Vagrant::VERSION) >= Gem::Version.new("1.2")
+        vagrant_version = Gem::Version.new(::Vagrant::VERSION)
+        if vagrant_version >= Gem::Version.new("1.5")
+          @ui = ::Vagrant::UI::Colored.new
+          @ui.opts[:target] = 'Butcher'
+        elsif vagrant_version >= Gem::Version.new("1.2")
           @ui = ::Vagrant::UI::Colored.new.scope('Butcher')
         else
           @ui = ::Vagrant::UI::Colored.new('Butcher')
