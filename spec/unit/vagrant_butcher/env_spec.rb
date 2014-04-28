a5c0338ee6519b42c5686e0e0b0ca03ca655ee53
Cassiano Leal <cassianoleal@gmail.com>
2014-04-28 21:11:02 +0100

    Fix specs for newer vagrant versions

--- spec/unit/vagrant_butcher/env_spec.rb
+++ spec/unit/vagrant_butcher/env_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper.rb'
 
 describe Vagrant::Butcher::Env do
+
   subject { described_class.new }
 
-  it "is a valid Vagrant UI object" do
-    subject.ui.should be_a Vagrant::UI::BasicScope
+  it "is a valid Vagrant 1.5+ UI object" do
+    subject.ui.should be_a(Vagrant::UI::Colored)
   end
+
 end
