c2bc739a456aba803cdc4775bcada8cacabe627c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-20 15:15:58 +0200

    Fix logger spam caused by Hashie used in Ridley
    See Hashie [issue](https://github.com/intridea/hashie/issues/394)
    Solution based on the Hashie recommendation to disable warnings:
    https://github.com/intridea/hashie#how-does-mash-handle-conflicts-with-pre-existing-methods
    
    Alternatively, completely silence the logger as done in Berkshelf:
    https://github.com/berkshelf/berkshelf/pull/1668/files
    ```
    Hashie.logger = Logger.new(nil)
    ```

--- lib/vagrant-butcher.rb
+++ lib/vagrant-butcher.rb
@@ -6,6 +6,23 @@ end
 require 'vagrant-butcher/version'
 require 'vagrant-butcher/errors'
 
+# Work around logger spam from hashie
+# https://github.com/intridea/hashie/issues/394
+begin
+  require "hashie"
+  require "hashie/logger"
+  # Based on Hashie's recommendation to disable warnings:
+  # https://github.com/intridea/hashie#how-does-mash-handle-conflicts-with-pre-existing-methods
+  class Response < Hashie::Mash
+    disable_warnings
+  end
+  # Alternatively, completely silence the logger as done in Berkshelf:
+  # https://github.com/berkshelf/berkshelf/pull/1668/files
+  # Hashie.logger = Logger.new(nil)
+rescue LoadError
+  # intentionally left blank
+end
+
 module Vagrant
   module Butcher
     autoload :Action,     'vagrant-butcher/action'
